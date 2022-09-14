acecf8efbb414b6a4874e7a9745c76ee7e712310
DHPARK <37052379+DHAPARK@users.noreply.github.com>
2022-09-14 18:12:14 +0900

    comp

--- pythonServer/mysite/polls/urls.py
+++ pythonServer/mysite/polls/urls.py
@@ -19,5 +19,6 @@ urlpatterns = [
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
-    path('modeling/polls/<int:modelData>/', views.viewModelData, name='index'),
+    #path('modeling/polls/<int:modelData>/', views.viewModelData, name='index'),
+    path('modeling/polls/<int:modelData>/', views.viewModelData),
 ]
